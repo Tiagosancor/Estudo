02e6649bfcbfc69638b3a62e47d4e02251c5da9e
Tiago Dos Santos Cordeiro <86391368+Tiagosancor@users.noreply.github.com>
2022-07-16 21:52:01 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_Pensionato/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Projeto_Pensionato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] aluno = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                aluno[i] = new Estudante (nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i <= n; i++)
+            {
+                if (aluno[i] != null)
+                {
+                    Console.WriteLine(i + ": " + aluno[i]);
+                }
+            }
+        }
+    }
+}
